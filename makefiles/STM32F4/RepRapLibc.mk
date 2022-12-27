468a39d72a19535e0d365f570024a80fc24a964e
Andy Shaw <andy-git@gloomy-place.com>
2022-12-27 18:02:13 +0000

    Updated for new source layout

--- makefiles/STM32F4/RepRapLibc.mk
+++ makefiles/STM32F4/RepRapLibc.mk
@@ -5,18 +5,19 @@ RRF_SRC_BASE  = $(REPRAPFIRMWARE_DIR)/src
 
 #Libc and libcpp in RRF
 RRFLIBC_SRC_DIRS = libc libcpp
-ifeq ($(CONFIG),IAP_BOOT_LOADER)
-RRFLIBC_SRC_DIRS += targets/STM32/FatFS
-endif
 RRFLIBC_SRC = $(addprefix $(RRF_SRC_BASE)/, $(RRFLIBC_SRC_DIRS))
 RRFLIBC_OBJ_SRC_C	  += $(foreach src, $(RRFLIBC_SRC), $(wildcard $(src)/*.c) ) 
 RRFLIBC_OBJ_SRC_CXX   += $(foreach src, $(RRFLIBC_SRC), $(wildcard $(src)/*.cpp) )
 RRFLIBC_OBJ_SRC_CC    += $(foreach src, $(RRFLIBC_SRC), $(wildcard $(src)/*.cc) )
-RRFLIBC_OBJ_SRC_C	  += $(RRF_SRC_BASE)/targets/common/FatFS/ff.c  $(RRF_SRC_BASE)/targets/common/FatFS/ffunicode.c
+RRFLIBC_OBJ_SRC_C	  += $(RRF_SRC_BASE)/Hardware/STM32/Libraries/Fatfs/ff.c  $(RRF_SRC_BASE)/Hardware/STM32/Libraries/Fatfs/ffunicode.c
+
+ifeq ($(CONFIG),IAP_BOOT_LOADER)
+RRFLIBC_OBJ_SRC_CXX += $(RRF_SRC_BASE)/Hardware/STM32/Libraries/Fatfs/SDCardSDIO.cpp $(RRF_SRC_BASE)/Hardware/STM32/Libraries/Fatfs/SDCardSPI.cpp $(RRF_SRC_BASE)/Hardware/STM32/Libraries/Fatfs/sd_mmc_wrapper.cpp
+endif
 
 RRFLIBC_INCLUDES = $(addprefix -I, $(RRFLIBC_SRC))
 ifeq ($(CONFIG),IAP_BOOT_LOADER)
-RRFLIBC_INCLUDES += -I$(RRF_SRC_BASE)/targets/common/FatFS/
+RRFLIBC_INCLUDES += -I$(RRF_SRC_BASE)/Hardware/STM32/Libraries/Fatfs/
 endif
 RRFLIBC_OBJS = $(patsubst %.c,$(BUILD_DIR)/%.o,$(RRFLIBC_OBJ_SRC_C)) $(patsubst %.cpp,$(BUILD_DIR)/%.o,$(RRFLIBC_OBJ_SRC_CXX))
 RRFLIBC_OBJS += $(patsubst %.cc,$(BUILD_DIR)/%.o,$(RRFLIBC_OBJ_SRC_CC))
