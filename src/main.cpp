0747a3e3058c78bd6bb11d3552c9ecab507d432b
Andy Shaw <andy-git@gloomy-place.com>
2025-10-08 14:49:58 +0100

    Fix build issues after 3.7 changes

--- src/main.cpp
+++ src/main.cpp
@@ -607,7 +607,8 @@ void SDInstallFirmware()
 	FATFS fs;
 #if STM32H7
 	alignas(4) static uint8_t sectorBuffer[512];
-	fs.win = sectorBuffer;
+	//fs.win = sectorBuffer;
+	ff_set_win(&fs, sectorBuffer);
 # endif
 	WriteLed(0, true);
 	if (MountSDCard(SDTYPE, &fs))
